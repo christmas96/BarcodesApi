b5a362287ce95617ec60608ff5792ae860261fc6
christmassngr <christmassngr@gmail.com>
2019-02-06 17:38:17 +0200

    Add files

---
+++ WebApplication1/Controllers/BarcodesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BarcodesController : Controller
+    {
+        private BarcodesEntities db = new BarcodesEntities();
+
+        // GET: Barcodes
+        public ActionResult Index()
+        {
+            return View(db.Barcodes.ToList());
+        }
+
+        // GET: Barcodes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Barcode barcode = db.Barcodes.Find(id);
+            if (barcode == null)
+            {
+                return HttpNotFound();
+            }
+            return View(barcode);
+        }
+
+        // GET: Barcodes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Barcodes/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Image,Code")] Barcode barcode)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Barcodes.Add(barcode);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(barcode);
+        }
+
+        // GET: Barcodes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Barcode barcode = db.Barcodes.Find(id);
+            if (barcode == null)
+            {
+                return HttpNotFound();
+            }
+            return View(barcode);
+        }
+
+        // POST: Barcodes/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Image,Code")] Barcode barcode)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(barcode).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(barcode);
+        }
+
+        // GET: Barcodes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Barcode barcode = db.Barcodes.Find(id);
+            if (barcode == null)
+            {
+                return HttpNotFound();
+            }
+            return View(barcode);
+        }
+
+        // POST: Barcodes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Barcode barcode = db.Barcodes.Find(id);
+            db.Barcodes.Remove(barcode);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Barcode> Get()
+        {
+            using (BarcodesEntities db = new BarcodesEntities())
+            {
+                return db.Barcodes.ToList();
+            }            
+        }
+
+        // GET api/values/5
+        public Barcode Get(string id)
+        {
+            string code = id;
+            Barcode barcode = new Barcode();
+            using (BarcodesEntities db = new BarcodesEntities())
+            {
+                barcode = db.Barcodes.Where(x => x.Code.Equals(code)).FirstOrDefault();
+            }
+            return barcode;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
